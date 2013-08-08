254831658e7217b70c6c100bdaa11242b5da0c38
Stian Johansen <stian.uansett@gmail.com>
2013-08-08 19:39:18 +0100

    Update yesyouwill.php

--- yesyouwill.php
+++ yesyouwill.php
@@ -1,10 +1,21 @@
 <?php
-$url = 'http://api.bike-stats.co.uk/service/rest/bikestats?format=json';
-$d = file_get_contents($url);
+$url = 'http://www.tfl.gov.uk/tfl/syndication/feeds/cycle-hire/livecyclehireupdates.xml';
+$d = Parse($url);
 
 if(isset($_GET['callback']))
 {
     header('Content-Type: text/javascript; charset=utf-8');
     echo $_GET['callback'] . '(' . $d . ');';
 }
+
+//Code from user bakkelun on Stackoverflow (http://stackoverflow.com/users/151237/bakkelun)
+public function Parse ($url) {
+        $fileContents= file_get_contents($url);
+        $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
+        $fileContents = trim(str_replace('"', "'", $fileContents));
+        $simpleXml = simplexml_load_string($fileContents);
+        $json = json_encode($simpleXml);
+
+        return $json;
+    }
 ?>
