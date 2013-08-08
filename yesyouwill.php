2209423f734f066130abf09a0730bcbf20d1c0d2
Stian Johansen <stian.uansett@gmail.com>
2013-08-08 19:46:14 +0100

    didnt work

--- yesyouwill.php
+++ yesyouwill.php
@@ -14,7 +14,7 @@ function Parse($url) {
         $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
         $fileContents = trim(str_replace('"', "'", $fileContents));
         $simpleXml = simplexml_load_string($fileContents);
-        $json = json_encode($simpleXml, JSON_PRETTY_PRINT);
+        $json = json_encode($simpleXml);
 
         return $json;
     }
