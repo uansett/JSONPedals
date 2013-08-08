a2006f8eabce672024ab45f7dc74cb636ac83413
Stian Johansen <stian.uansett@gmail.com>
2013-08-08 19:44:47 +0100

    Its prettier

--- yesyouwill.php
+++ yesyouwill.php
@@ -14,7 +14,7 @@ function Parse($url) {
         $fileContents = str_replace(array("\n", "\r", "\t"), '', $fileContents);
         $fileContents = trim(str_replace('"', "'", $fileContents));
         $simpleXml = simplexml_load_string($fileContents);
-        $json = json_encode($simpleXml);
+        $json = json_encode($simpleXml, JSON_PRETTY_PRINT);
 
         return $json;
     }
